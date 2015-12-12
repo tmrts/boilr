88cc83161c66ab7c98db8bade405e81af89eb461
Tamer Tas <contact@tmrts.com>
2015-12-12 15:23:11 +0200

    Add input logging utilities

---
+++ pkg/util/tlog/log.go
@@ -0,0 +1,28 @@
+// Package flog implements logging utilities for tmplt
+package tlog
+
+import "github.com/Sirupsen/logrus"
+
+func Debug(msg string) {
+	logrus.Debug(msg)
+}
+
+func Info(msg string) {
+	logrus.Info(msg)
+}
+
+func Warn(msg string) {
+	logrus.Warning(msg)
+}
+
+func Error(msg string) {
+	logrus.Error(msg)
+}
+
+func Fatal(msg string) {
+	logrus.Fatal(msg)
+}
+
+func Panic(msg string) {
+	logrus.Panic(msg)
+}
