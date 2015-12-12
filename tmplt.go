180dbd95e9559c3f5c699b760cc2f3a0f57adada
Tamer Tas <contact@tmrts.com>
2015-12-12 21:02:46 +0200

    Trim down main pkg

--- tmplt.go
+++ tmplt.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	cli "github.com/spf13/cobra"
-
-	"github.com/tmrts/tmplt/pkg/cmd"
-)
+import "github.com/tmrts/tmplt/pkg/cmd"
 
 func main() {
-	mainCmd := &cli.Command{
-		Use: "tmplt",
-	}
-
-	mainCmd.AddCommand(cmd.Use)
-	mainCmd.AddCommand(cmd.Save)
-	mainCmd.AddCommand(cmd.Verify)
-	mainCmd.AddCommand(cmd.Version)
-
-	mainCmd.Execute()
+	cmd.Run()
 }
