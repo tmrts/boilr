b830cb6ff2c9a1592e6f219c3b532f677c6ed472
Tamer Tas <contact@tmrts.com>
2016-01-02 09:29:34 +0200

    Create a sub-command for template related commands

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -13,31 +13,39 @@ func Run() {
 	// TODO use command factories instead of global command variables
 	// TODO add describe command that shows template metadata information
 	// TODO add create command that creates a minimal template template
-	// TODO rename template-name to template-tag
+	// TODO rename template-tag to template-tag
+
+	Template := &cli.Command{
+		Use:   "template",
+		Short: "Run a template command",
+	}
+
+	Template.AddCommand(Delete)
+
+	Download.PersistentFlags().BoolP("force", "f", false, "Overwrite existing template with the same name")
+	Template.AddCommand(Download)
+
+	List.PersistentFlags().BoolP("dont-prettify", "", false, "Print only the template names without fancy formatting")
+	Template.AddCommand(List)
+
+	Template.AddCommand(Rename)
+
+	Save.PersistentFlags().BoolP("force", "f", false, "Overwrite existing template with the same name")
+	Template.AddCommand(Save)
+
+	Use.PersistentFlags().BoolP("use-defaults", "f", false, "Uses default values in project.json instead of prompting the user")
+	Template.AddCommand(Use)
+
+	Template.AddCommand(Validate)
+
 	Init.PersistentFlags().BoolP("force", "f", false, "Recreate directories if they exist")
 	Root.AddCommand(Init)
 
 	Root.AddCommand(ConfigureBashCompletion)
 
-	Root.AddCommand(Delete)
-
-	Download.PersistentFlags().BoolP("force", "f", false, "Overwrite existing template with the same name")
-	Root.AddCommand(Download)
-
-	List.PersistentFlags().BoolP("dont-prettify", "", false, "Print only the template names without fancy formatting")
-	Root.AddCommand(List)
-
-	Root.AddCommand(Rename)
-
 	Root.AddCommand(Report)
 
-	Save.PersistentFlags().BoolP("force", "f", false, "Overwrite existing template with the same name")
-	Root.AddCommand(Save)
-
-	Use.PersistentFlags().BoolP("use-defaults", "f", false, "Uses default values in project.json instead of prompting the user")
-	Root.AddCommand(Use)
-
-	Root.AddCommand(Validate)
+	Root.AddCommand(Template)
 
 	Root.AddCommand(Version)
 
