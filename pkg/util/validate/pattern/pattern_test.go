3c19a4d464c4d0ad9b1e1d5c4d0b310977808284
Tamer Tas <contact@tmrts.com>
2016-02-11 07:28:16 +0200

    Extend the tests for regexp pattern package

--- pkg/util/validate/pattern/pattern_test.go
+++ pkg/util/validate/pattern/pattern_test.go
@@ -26,3 +26,67 @@ func TestUnixPathPattern(t *testing.T) {
 		}
 	}
 }
+
+func TestAlphanumericPattern(t *testing.T) {
+	tests := []struct {
+		String string
+		Valid  bool
+	}{
+		{" ", false},
+		{"/", false},
+		{"root", true},
+		{"tmp-dir", false},
+		{"TMPDIR", true},
+		{"L33T", true},
+		{"L@@T", false},
+	}
+
+	for _, test := range tests {
+		if ok := pattern.Alphanumeric.MatchString(test.String); ok != test.Valid {
+			t.Errorf("pattern.Alphanumeric.MatchString(%q) expected to be %v", test.String, test.Valid)
+		}
+	}
+}
+
+func TestIntegerPattern(t *testing.T) {
+	tests := []struct {
+		String string
+		Valid  bool
+	}{
+		{"", false},
+		{" ", false},
+		{"/", false},
+		{"root", false},
+		{"L33T", false},
+		{"", false},
+	}
+
+	for _, test := range tests {
+		if ok := pattern.Numeric.MatchString(test.String); ok != test.Valid {
+			t.Errorf("pattern.Numeric.MatchString(%q) expected to be %v", test.String, test.Valid)
+		}
+	}
+}
+
+func TestURLPattern(t *testing.T) {
+	tests := []struct {
+		String string
+		Valid  bool
+	}{
+		{"", false},
+		{" ", false},
+		{"/", false},
+		{"http://", false},
+		{"http://github.com/tmrts/boilr", true},
+		{"https://github.com/tmrts/boilr", true},
+		{"github.com/tmrts/boilr", true},
+		{"rawcontent.github.com/tmrts/boilr", true},
+		{"github.com:80/tmrts/boilr", true},
+	}
+
+	for _, test := range tests {
+		if ok := pattern.URL.MatchString(test.String); ok != test.Valid {
+			t.Errorf("pattern.URL.MatchString(%q) expected to be %v", test.String, test.Valid)
+		}
+	}
+}
