4fbf60dbbbdca721e7f6730c9b1e0c336f4bc00d
Tamer Tas <contact@tmrts.com>
2016-05-24 09:19:21 +0300

    Implement human readable file size template function

--- pkg/template/functions.go
+++ pkg/template/functions.go
@@ -3,6 +3,7 @@ package template
 import (
 	"fmt"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 	"text/template"
@@ -30,6 +31,50 @@ var (
 
 			return fmt.Sprintf("%b", n)
 		},
+
+		"formatFilesize": func(value interface{}) string {
+			var size float64
+
+			v := reflect.ValueOf(value)
+			switch v.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				size = float64(v.Int())
+			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+				size = float64(v.Uint())
+			case reflect.Float32, reflect.Float64:
+				size = v.Float()
+			default:
+				return ""
+			}
+
+			var KB float64 = 1 << 10
+			var MB float64 = 1 << 20
+			var GB float64 = 1 << 30
+			var TB float64 = 1 << 40
+			var PB float64 = 1 << 50
+
+			filesizeFormat := func(filesize float64, suffix string) string {
+				return strings.Replace(fmt.Sprintf("%.1f %s", filesize, suffix), ".0", "", -1)
+			}
+
+			var result string
+			if size < KB {
+				result = filesizeFormat(size, "bytes")
+			} else if size < MB {
+				result = filesizeFormat(size/KB, "KB")
+			} else if size < GB {
+				result = filesizeFormat(size/MB, "MB")
+			} else if size < TB {
+				result = filesizeFormat(size/GB, "GB")
+			} else if size < PB {
+				result = filesizeFormat(size/TB, "TB")
+			} else {
+				result = filesizeFormat(size/PB, "PB")
+			}
+
+			return result
+		},
+
 		// String utilities
 		"toLower": strings.ToLower,
 		"toUpper": strings.ToUpper,
