f44b913f7fff9d6865367f00b1bbf9ebb5243a12
David McKay <david@rawkode.com>
2016-06-12 09:34:03 +0100

    Removing arbitrary zip/master check and allowing any zip definition

--- pkg/host/github.go
+++ pkg/host/github.go
@@ -1,5 +1,6 @@
 package host
 
+import "regexp"
 import "strings"
 
 // ZipURL returns the URL of the zip archive given a github repository URL.
@@ -9,9 +10,10 @@ func ZipURL(url string) string {
 	url = strings.TrimSuffix(url, "/")
 	url = strings.TrimPrefix(url, "/")
 
-	if strings.HasSuffix(url, "zip/master") {
-		return url
-	}
+    zipRegex, _ := regexp.Compile(`zip/(\S+)$`)
+    if zipRegex.MatchString(url) {
+        return url
+    }
 
 	// So this could identify a port number, but since we only support github
 	// I don't believe using it as a version modifier is a problem. Though
