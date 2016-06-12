72c36cb13246363699e377ffe13bd8f6e2ec85d1
David McKay <david@rawkode.com>
2016-06-12 09:24:55 +0100

    Allowing a version/branch string to be parsed in download url

--- pkg/host/github.go
+++ pkg/host/github.go
@@ -4,6 +4,8 @@ import "strings"
 
 // ZipURL returns the URL of the zip archive given a github repository URL.
 func ZipURL(url string) string {
+	var version = "master"
+
 	url = strings.TrimSuffix(url, "/")
 	url = strings.TrimPrefix(url, "/")
 
@@ -11,6 +13,18 @@ func ZipURL(url string) string {
 		return url
 	}
 
+	// So this could identify a port number, but since we only support github
+	// I don't believe using it as a version modifier is a problem. Though
+	// perhaps we should use something else instead?
+	if strings.Contains(url, ":") {
+		parts := strings.SplitAfter(url, ":")
+
+		url = parts[0]
+		version = parts[1]
+
+		url = strings.TrimSuffix(url, ":")
+	}
+
 	// BUG filepath.Join trims slashes use url.Join
-	return "https://codeload.github.com/" + url + "/zip/master"
+	return "https://codeload.github.com/" + url + "/zip/" + version
 }
