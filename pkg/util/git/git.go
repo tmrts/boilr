2aea094ac8deefe12a4faceacb1bbdc33f25f623
Tamer Tas <contact@tmrts.com>
2017-02-27 11:51:58 +0300

    pkg/util/git: import util pkg for git methods

---
+++ pkg/util/git/git.go
@@ -0,0 +1,15 @@
+// Package git is a facade for git methods used by boilr
+package git
+
+import git "srcd.works/go-git.v4"
+
+// CloneOptions are used when cloning a git repository
+type CloneOptions git.CloneOptions
+
+// Clone clones a git repository with the given options
+func Clone(dir string, opts CloneOptions) error {
+	o := git.CloneOptions(opts)
+
+	_, err := git.PlainClone(dir, false, &o)
+	return err
+}
