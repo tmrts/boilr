25bf3bfedd94101c2a27a63aef5a932e7b5c172a
David McKay <david@rawkode.com>
2017-03-04 10:05:25 +0000

    *: init boilr automatically
    Signed-off-by: David McKay <david@rawkode.com>

--- boilr.go
+++ boilr.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/tmrts/boilr/pkg/cmd"
+import (
+  "fmt"
+  "github.com/tmrts/boilr/pkg/boilr"
+  "github.com/tmrts/boilr/pkg/cmd"
+  "github.com/tmrts/boilr/pkg/util/exit"
+  "github.com/tmrts/boilr/pkg/util/osutil"
+)
 
 func main() {
-	cmd.Run()
+  if exists, err := osutil.DirExists(boilr.Configuration.TemplateDirPath); ! exists {
+    if err := osutil.CreateDirs(boilr.Configuration.TemplateDirPath); err != nil {
+      exit.Error(fmt.Errorf("Tried to initialise your template directory, but it has failed: %s", err))
+    }
+  } else if err != nil {
+    exit.Error(fmt.Errorf("Failed to init: %s", err))
+  }
+
+  cmd.Run()
 }
+
